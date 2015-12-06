26c6c600729261978f01bfcf4c684f65c56dec1a
Vladimír Matěna <matena@d3s.mff.cuni.cz>
2015-12-06 11:54:46 +0100

    Add missing CDEECo application definitions

--- src/ics_app.cpp
+++ src/ics_app.cpp
@@ -3,8 +3,25 @@
  */
  
 #include <cstdint>
+
+#include "MrfRadio.h"
+#include "Vehicle.h"
+#include "ICS.h"
+#include "SpeedExchange.h"
  
 void cdeecoSetup(const uint32_t uniqId) {
-	
+	//// System setup
+	auto radio = new MrfRadio(0, uniqId, uniqId);
+	auto system = new CDEECO::System<3, 64>(*radio);
+
+	// ICS component
+	auto ics = new ICS::Component(*system, uniqId);
+
+	// Caches
+	auto vehicleCache = new CDEECO::KnowledgeCache<Vehicle::Component::Type, Vehicle::Knowledge, 10>();
+	system->registerCache(vehicleCache);
+
+	// Ensembles
+	new SpeedExchange::Ensemble(*ics, *vehicleCache);
 }
 
--- src/vehicle_app.cpp
+++ src/vehicle_app.cpp
@@ -3,8 +3,25 @@
  */
  
 #include <cstdint>
+
+#include "MrfRadio.h"
+#include "Vehicle.h"
+#include "ICS.h"
+#include "SpeedExchange.h"
  
 void cdeecoSetup(const uint32_t uniqId) {
-	
-}
+	//// System setup
+	auto radio = new MrfRadio(0, uniqId, uniqId);
+	auto system = new CDEECO::System<3, 64>(*radio);
 
+	// Vehicle component
+	auto vehicleInterface = new VehicleInterface();
+	auto vehicle = new Vehicle::Component(*system, uniqId, *vehicleInterface);
+
+	// Caches
+	auto icsCache = new CDEECO::KnowledgeCache<ICS::Component::Type, ICS::Knowledge, 10>();
+	system->registerCache(icsCache);
+
+	// Ensembles
+	new SpeedExchange::Ensemble(*vehicle, *icsCache);
+}
